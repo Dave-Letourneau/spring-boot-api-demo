059ec56a5a406151cd3ba17ea5974dd268dd12d6
David Letourneau <deldel@suddenlink.net>
2021-05-17 20:07:28 -0500

    First commit with Spring boot app.

---
+++ src/test/java/com/springapi/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springapi.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
